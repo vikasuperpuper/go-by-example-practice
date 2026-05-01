3c837f084926ce879cbb98a95f2e1c5951f75358
valyamoro <moralek228@gmail.com>
2026-05-01 17:57:39 +0300

    .\02-values\gopherchan2006\values.go

--- 02-values/gopherchan2006/values.go
+++ 02-values/gopherchan2006/values.go
@@ -1,5 +1,14 @@
 ﻿package main
 
+import "fmt"
+
 func main() {
-\t// TODO: переписанный пример темы values
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
