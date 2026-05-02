14ca6fc01637ef60c85ae39fd1913281c7ee4142
valyamoro <moralek228@gmail.com>
2026-05-02 18:32:11 +0300

    03-variables/gopherchan2006/variables.go

--- 03-variables/gopherchan2006/variables.go
+++ 03-variables/gopherchan2006/variables.go
@@ -1,5 +1,20 @@
 ﻿package main
 
+import "fmt"
+
 func main() {
-\t// TODO: переписанный пример темы variables
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
 }
